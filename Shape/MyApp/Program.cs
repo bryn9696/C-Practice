6f7c7008591e06ceff9a03a131cb330070c3e5e5
bryn9696 <bryn2009@live.co.uk>
2022-08-23 14:34:21 +0100

    draw simple shape

---
+++ Shape/MyApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Shape
+{
+  class Program
+  {
+    static void Main(string[] args)
+      {
+        Console.WriteLine("   /|");
+        Console.WriteLine("  / |");
+        Console.WriteLine(" /  |");
+        Console.WriteLine("/___|");
+      }
+  }
+}
