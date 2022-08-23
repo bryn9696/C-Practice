7bb19bdcd268d9aa65cf8fbc1635b76e56a2db62
bryn9696 <bryn2009@live.co.uk>
2022-08-23 14:44:14 +0100

    looking at how to create variables

---
+++ Variables/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+      {
+
+        string characterName = "Bryn";
+        int characterAge;
+        characterAge = 26;
+
+        Console.WriteLine("My name is " + characterName + ", I am " + characterAge + " years old.");
+      }
+  }
+}
