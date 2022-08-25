b18198c3bfb1ee6403395a47489d634c29f6b857
bryn9696 <bryn2009@live.co.uk>
2022-08-25 11:22:50 +0100

    looking at different datatypes

---
+++ DataTypes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace DataTypes
+{
+  class Program
+  {
+    static void Main(string[] args)
+      {
+        string phrase = "Hello Bryn";
+        char grade = 'A';
+        int age = 26;
+        double gpa = 3.2;
+        bool isMale = true;
+
+      }
+  }
+}
