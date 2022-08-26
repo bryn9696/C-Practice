93bd72535453102ee3f46312945b084438acceae
bryn9696 <bryn2009@live.co.uk>
2022-08-26 12:00:57 +0100

    using strings in C#

---
+++ Strings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace String
+{
+  class Program
+  {
+    static void Main(string[] args)
+      {
+        Console.WriteLine("Hello, \nBryn");
+        Console.WriteLine("Hello, \nBryn" + " is cool");
+        string phrase = "Brynnnnn";
+        Console.WriteLine( phrase );
+        Console.WriteLine( phrase.Length );
+        Console.WriteLine( phrase.ToUpper() );
+        Console.WriteLine( phrase.Contains("Bryn") );
+        Console.WriteLine( phrase[2] );
+        Console.WriteLine( phrase.IndexOf('r') );
+        Console.WriteLine( phrase.Substring(3) );
+      }
+  }
+}
