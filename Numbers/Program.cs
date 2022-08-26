951190eed2f06ac98fd151e478e1df306eeb5f62
bryn9696 <bryn2009@live.co.uk>
2022-08-26 14:10:20 +0100

    working with numbers

---
+++ Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+using System;
+namespace String
+{
+  class Program
+  {
+    static void Main(string[] args)
+      {
+        int num = 4;
+        num++;
+
+        int x = 4;
+        x--;
+
+        Console.WriteLine( num );
+        Console.WriteLine( x );
+        Console.WriteLine( Math.Pow(4, 2) );
+        Console.WriteLine( Math.Sqrt(64) );
+        Console.WriteLine( Math.Max(4, 20) );
+        Console.WriteLine( Math.Round(4.69) );
+      }
+  }
+}
